d0254730c90652bfdffa19a0c3795c2cf457a61e
VozzRoman <80773157+VozzRoman@users.noreply.github.com>
2021-03-25 23:39:59 +0200

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,26 @@
+
+//------------------burger--------------------//
+$(document).ready(function () {
+	$('.header__burger').click(function (event) {
+		$('.header__burger,.header__menu').toggleClass('active');
+		$('body').toggleClass('lock');
+
+	});
+});
+
+//-------------------ibg---------------------//
+function ibg() {
+	let ibg = document.querySelectorAll(".ibg");
+	for (var i = 0; i < ibg.length; i++) {
+		if (ibg[i].querySelector('img')) {
+			ibg[i].style.backgroundImage = 'url(' + ibg[i].querySelector('img').getAttribute('src') + ')';
+		}
+	}
+}
+ibg();
+
+//------------------slider---------------//
+
+window.addEventListener('load', function () {
+	baguetteBox.run('.gallery');
+});
